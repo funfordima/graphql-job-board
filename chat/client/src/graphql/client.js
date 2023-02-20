1966bf9632aba7f93ae8fb4b4baf3019cb87660e
Dima Litvinov <dlv@vava.cars>
2023-02-20 17:33:22 +0200

    feat: add chat application, use query subscriptio

---
+++ chat/client/src/graphql/client.js
@@ -0,0 +1,32 @@
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient as createWsClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+import { getAccessToken } from '../auth';
+
+const GRAPHQL_URL = 'http://localhost:9000/graphql';
+
+const httpLink = new HttpLink({
+  uri: GRAPHQL_URL,
+});
+
+const isSubscription = ({ query }) => {
+  const definition = getMainDefinition(query);
+
+  return definition.kind === Kind.OPERATION_DEFINITION && definition.operation === OperationTypeNode.SUBSCRIPTION;
+};
+
+const wsLink = new GraphQLWsLink(createWsClient({
+  url: 'ws://localhost:9000/graphql',
+  connectionParams: () => ({
+    accessToken: getAccessToken(),
+  }),
+}));
+
+export const client = new ApolloClient({
+  link: split(isSubscription, wsLink, httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
